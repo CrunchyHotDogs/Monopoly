5f835841d71bf23eaf4d39e3fc8c195d85e27554
CrunchyHotDogs <Kcross119@yahoo.ca>
2015-02-23 11:26:44 -0500

    Removed all of the template licenses from the top of files.

--- src/monopoly/MainMenu.java
+++ src/monopoly/MainMenu.java
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package monopoly;
 
 import java.io.File;
-import java.util.Arrays;
 import javax.swing.JButton;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
